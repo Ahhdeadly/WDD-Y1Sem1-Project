9d526576c32d7c2ac54d27b8e37c4c7299ecffa7
Merlin <merlinmussen@gmail.com>
2023-11-30 10:29:40 +0000

    second commit

---
+++ Javascript/external.js
@@ -0,0 +1,14 @@
+//external js file - save as external.js is changing anything
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
